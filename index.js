169ca9d5cde22c6bbb14b47c17d7334533030eec
Emily Jirles <emily.jirles@gmail.com>
2018-07-11 19:59:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ function findMatching(collection, name){
   const capitalizedName = name[0].toUpperCase() + name.slice(1)
   const lowerCaseName = name.toLowerCase()
 
-  return collection.filter(name => name === newName );
+  return collection.filter(name => name === capitalizedName || name ===lowerCaseName );
 };
