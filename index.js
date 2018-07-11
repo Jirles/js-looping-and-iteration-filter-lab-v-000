fd8c71f27bdddc68e70d3c10ac60678d4c941c5a
Emily Jirles <emily.jirles@gmail.com>
2018-07-11 19:57:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 // Code your solution in this file
 function findMatching(collection, name){
-  const newCollection = [];
-  const newName = name.toLowerCase();
-  for (const name of collection) {
-    newCollection.push(name.toLowerCase())
-  };
+  const newName = name[0].toUpperCase() + name.slice(1)
+
   return newCollection.filter(name => name === newName );
 };
