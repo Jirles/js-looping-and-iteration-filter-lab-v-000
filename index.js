816b07ba554f8f5d4cdaed373d3cb5f2ef2397d9
Emily Jirles <emily.jirles@gmail.com>
2018-07-11 19:48:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 // Code your solution in this file
 function findMatching(collection, name){
-
+  const newCollection = [];
+  const newName = toLowerCase(name);
+  for (const name of collection)
+  return collection.filter(function() { return })
 };
