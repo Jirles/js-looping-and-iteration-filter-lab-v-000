a17e47e2b84b4c8fd642449664f451a15f1c09f4
Emily Jirles <emily.jirles@gmail.com>
2018-07-11 20:04:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Code your solution in this file
 function findMatching(collection, name){
+  const lowerCaseName = name.toLowerCase();
   return collection.filter(driver => driver === name || driver ===lowerCaseName );
 };
 
