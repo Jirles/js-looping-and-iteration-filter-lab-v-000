a665e60ac4e0e2777b6baa96df052864610749cd
Emily Jirles <emily.jirles@gmail.com>
2018-07-11 20:04:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solution in this file
 function findMatching(collection, name){
   const lowerCaseName = name.toLowerCase();
-  return collection.filter(driver => driver === name || driver ===lowerCaseName );
+  return collection.filter(driver => driver === name || driver === lowerCaseName);
 };
 
 function fuzzyMatch(collection, letters){
