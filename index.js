be3701bc7a17a2f7a606d6c70e53eaeccc38b212
Emily Jirles <emily.jirles@gmail.com>
2018-07-11 20:04:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // Code your solution in this file
 function findMatching(collection, name){
-
-  return collection.filter(driver => driver === name || name ===lowerCaseName );
+  return collection.filter(driver => driver === name || driver ===lowerCaseName );
 };
 
 function fuzzyMatch(collection, letters){
