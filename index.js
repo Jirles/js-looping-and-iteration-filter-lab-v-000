7db2a51bd2aed75aeef730cd5bb76e90b0407d2b
Emily Jirles <emily.jirles@gmail.com>
2018-07-11 20:00:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solution in this file
 function findMatching(collection, name){
-  const capitalizedName = name[0].toUpperCase() + name.slice(1)
-  const lowerCaseName = name.toLowerCase()
+  const capitalizedName = name[0].toUpperCase() + name.slice(1);
+  const lowerCaseName = name.toLowerCase();
 
   return collection.filter(name => name === capitalizedName || name ===lowerCaseName );
 };
